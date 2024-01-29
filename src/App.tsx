1117561105ffe07b49584be9cae5d3bd9b261411
Luy Vannda <philips.joe329@gmail.com>
2024-01-29 11:07:16 +0700

    learn recoil and mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import "./App.css";
+import Pricing from "./components/Pricing";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Pricing />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import { Button, Link, Typography } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+
+import Toolbar from "@mui/material/Toolbar";
+
+export default function Header() {
+  return (
+    <AppBar
+      position="static"
+      color="default"
+      elevation={0}
+      sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+    >
+      <Toolbar sx={{ flexWrap: "wrap" }}>
+        <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+          Company name
+        </Typography>
+        <nav>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Features
+          </Link>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Enterprise
+          </Link>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Support
+          </Link>
+        </nav>
+        <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
